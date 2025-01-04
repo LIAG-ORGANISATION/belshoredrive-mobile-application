c43bf2df714d1b77953f00a968d71d95e1b7b40f
David <david@kamealabs.com>
2025-01-04 13:57:18 +0100

    feat: add support storybook with biome

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,15 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
-  
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+const withStorybook = require("@storybook/react-native/metro/withStorybook");
+
 const config = getDefaultConfig(__dirname);
-  
-module.exports = withNativeWind(config, { input: './global.css' });
+
+// Chain the withNativeWind and withStorybook configurations
+module.exports = withStorybook(
+  withNativeWind(config, { input: "./src/global.css" }),
+  {
+    enabled: true,
+    configPath: path.resolve(__dirname, "./.storybook"),
+  },
+);
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,31 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import StorybookUIRoot from "../../.storybook";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+// import Constants from "expo-constants";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+
+const isStoryBookEnabled = true;
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -25,7 +33,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -44,17 +52,30 @@ export default function RootLayout() {
     return null;
   }
 
-  return <GluestackUIProvider mode="light"><RootLayoutNav /></GluestackUIProvider>;
+  return (
+    <GluestackUIProvider mode="light">
+      <RootLayoutNav />
+    </GluestackUIProvider>
+  );
 }
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
+  if (isStoryBookEnabled) {
+    return (
+      <GluestackUIProvider mode="light">
+        <StorybookUIRoot />
+      </GluestackUIProvider>
+    );
+  }
+
   return (
-    <GluestackUIProvider mode="light"><ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode="light">
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
         </Stack>
       </ThemeProvider>
     </GluestackUIProvider>
