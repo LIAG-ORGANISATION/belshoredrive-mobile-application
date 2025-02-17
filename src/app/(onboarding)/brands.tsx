7b94c82a3b830dbbbaabcbb6e8b30ae40d5c7b8a
David <david@kamealabs.com>
2025-02-17 21:25:20 +0100

    refactor: profiles can now be updates from everywhere

--- src/app/(onboarding)/brands.tsx
+++ src/app/(onboarding)/brands.tsx
@@ -1,94 +1,14 @@
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 
-import { valibotResolver } from "@hookform/resolvers/valibot";
-import { useForm } from "react-hook-form";
-
-import { ChipSelector } from "@/components/chip-selector";
-import { Button } from "@/components/ui/button";
-import { SkeletonText } from "@/components/ui/skeleton-text";
-import type { ExtractId } from "@/lib/helpers/extract-id";
-import { mapToId } from "@/lib/helpers/map-to-id";
-import {
-  type FavoriteBrandsType,
-  favoriteBrands,
-} from "@/lib/schemas/onboarding";
-import { type BrandsType, useFetchBrands } from "@/network/brands";
-import {
-  useFetchUserProfile,
-  useUpdateUserProfile,
-} from "@/network/user-profile";
 import { router } from "expo-router";
-export default function Onboarding() {
-  const {
-    data: brands = [],
-    isLoading: loadingBrands,
-    error: brandsError,
-  } = useFetchBrands();
-  const { data: profile, isLoading: loadingProfile } = useFetchUserProfile();
-  const { mutate: updateProfile } = useUpdateUserProfile();
 
-  const {
-    control,
-    reset,
-    handleSubmit,
-    formState: { isValid, isSubmitting },
-  } = useForm<FavoriteBrandsType>({
-    resolver: valibotResolver(favoriteBrands),
-  });
+import { UpdateBrands } from "@/components/user-details/update-brands";
 
-  const onSubmit = async (data: FavoriteBrandsType) => {
-    try {
-      await updateProfile({
-        favorite_vehicle_brands: data.favorite_vehicle_brands,
-      });
-      router.push("/(onboarding)/interests");
-    } catch (error) {
-      // Revert selection if update fails
-      console.error("Failed to update favorite brands:", error);
-    }
-  };
-
-  useEffect(() => {
-    reset(profile || {});
-  }, [profile]);
-
-  if (brandsError) return <Text>Error: {brandsError.message}</Text>;
-
+export default function OnboardingBrands() {
   return (
     <View className="flex-1 bg-black relative px-2">
-      <Text className="text-white text-2xl font-bold py-4">
-        Quelles sont tes marques préférées ?
-      </Text>
-
-      {/* TODO: Make it as generic components for all screen that needs this */}
-      {loadingBrands || loadingProfile ? (
-        <View className="flex flex-row flex-wrap gap-2 pb-4">
-          {[...Array(10)].map((_, index) => (
-            <SkeletonText key={index} />
-          ))}
-        </View>
-      ) : (
-        <>
-        <View className="flex-1">
-          <ChipSelector<FavoriteBrandsType, ExtractId<BrandsType, "brand_id">>
-            name="favorite_vehicle_brands"
-            control={control}
-            items={mapToId(brands, "brand_id")}
-            haveSearch={true}
-          />
-      </View>
-
-      <View className="absolute bottom-0 w-full px-4 pb-10 pt-4 bg-black z-50 inset-x-0">
-        <Button
-          variant="secondary"
-            label="Continuer"
-            disabled={!isValid || isSubmitting}
-            onPress={handleSubmit(onSubmit)}
-            />
-          </View>
-        </>
-      )}
+      <UpdateBrands title="Quelles sont tes marques préférées ?" onSubmitCallback={() => router.push("/(onboarding)/interests")} />
     </View>
   );
 }
--- src/app/(onboarding)/index.tsx
+++ src/app/(onboarding)/index.tsx
@@ -1,88 +1,15 @@
-import { valibotResolver } from "@hookform/resolvers/valibot";
 import { router } from "expo-router";
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
-import { ChipSelector } from "@/components/chip-selector";
-import { Button } from "@/components/ui/button";
-import type { ExtractId } from "@/lib/helpers/extract-id";
-import { mapToId } from "@/lib/helpers/map-to-id";
-import {
-  type RegionAndDepartmentsType,
-  regionsAndDepartments,
-} from "@/lib/schemas/onboarding";
-import {
-  type DepartmentType,
-  useFetchDepartments,
-} from "@/network/departments";
-import {
-  useFetchUserProfile,
-  useUpdateUserProfile,
-} from "@/network/user-profile";
+import { UpdateDepartments } from "@/components/user-details/update-departments";
 
-export default function Onboarding() {
-  const {
-    data: departments = [],
-    isLoading: loadingDepts,
-    error: deptsError,
-  } = useFetchDepartments();
-  const { data: profile, isLoading: loadingProfile } = useFetchUserProfile();
-  const { mutate: updateProfile } = useUpdateUserProfile();
-
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { isValid, isSubmitting },
-  } = useForm<RegionAndDepartmentsType>({
-    resolver: valibotResolver(regionsAndDepartments),
-  });
-
-  // // Initialize selected departments from profile
-
-  const onSubmit = async (data: RegionAndDepartmentsType) => {
-    try {
-      await updateProfile(data);
-      router.push("/(onboarding)/brands");
-    } catch (error) {
-      console.error("Failed to update viewable departments:", error);
-    }
-  };
-
-  useEffect(() => {
-    reset(profile || {});
-  }, [profile]);
-
-  if (loadingDepts || loadingProfile) return <Text>Loading...</Text>;
-  if (deptsError) return <Text>Error: {deptsError.message}</Text>;
-
+export default function OnboardingDepartments() {
   return (
     <View className="flex-1 bg-black relative px-2">
-      <Text className="text-white text-2xl font-bold py-4">
-        Dans quelle(s) région(s) peut-on vous croiser ?{" "}
-      </Text>
-
-      <View className="flex-1">
-        <ChipSelector<
-          RegionAndDepartmentsType,
-          ExtractId<DepartmentType, "department_id">
-        >
-          name="viewable_departments"
-          control={control}
-          items={mapToId(departments, "department_id")}
-          haveSearch={true}
-        />
-      </View>
-
-      <View className="absolute bottom-0 w-full px-4 pb-10 pt-4 bg-black z-50 inset-x-0">
-        <Button
-          variant="secondary"
-          label="Continuer"
-          disabled={!isValid || isSubmitting}
-          onPress={handleSubmit(onSubmit)}
-        />
-      </View>
+      <UpdateDepartments
+        title="Dans quelle(s) région(s) peut-on vous croiser ?"
+        onSubmitCallback={() => router.push("/(onboarding)/brands")}
+      />
     </View>
   );
 }
