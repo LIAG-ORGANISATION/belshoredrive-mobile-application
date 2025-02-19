0d3bc9682be0def490cc776677c7b1dfd75dfefe
David <david@kamealabs.com>
2025-02-19 18:27:02 +0100

    fiix: completely remove storybook from package and config

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,8 @@
 const path = require("node:path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const withStorybook = require("@storybook/react-native/metro/withStorybook");
 
 const config = getDefaultConfig(__dirname);
 
 // Chain the withNativeWind and withStorybook configurations
-module.exports = withStorybook(
-	withNativeWind(config, { input: "./src/global.css" }),
-	{
-		enabled: true,
-		configPath: path.resolve(__dirname, "./.storybook"),
-	},
-);
+module.exports = withNativeWind(config, { input: "./src/global.css" });
