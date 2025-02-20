c88e3ce46b7ad8ba7c15e8380165277f1db2fb09
David <david@kamealabs.com>
2025-02-20 19:57:58 +0100

    fix: trying to add the default font :cry:

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,7 +10,7 @@ import { useFonts } from "expo-font";
 import { Stack, router } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import { AppState } from "react-native";
+import { AppState, View, } from "react-native";
 
 export { ErrorBoundary } from "expo-router";
 
@@ -84,213 +84,215 @@ function RootLayoutNav() {
 	}, []);
 
 	return (
-		<QueryClientProvider client={queryClient}>
-			<ThemeProvider value={DefaultTheme}>
-				<Stack>
-					<Stack.Screen
-						name="index"
-						options={{ headerShown: false, headerTitle: "" }}
-					/>
-					<Stack.Screen
-						name="auth/index"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: "",
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => router.back()}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="auth/login"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: "",
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => router.back()}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="auth/phone"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={0} totalSteps={4} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => router.back()}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="auth/email"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={0} totalSteps={4} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => router.back()}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="auth/verification"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={1} totalSteps={4} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => router.back()}
-								/>
-							),
-						}}
-					/>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider value={DefaultTheme}>
+					<View className="flex-1 bg-black font-sans">
+						<Stack>
+							<Stack.Screen
+								name="index"
+								options={{ headerShown: false, headerTitle: "" }}
+							/>
+							<Stack.Screen
+								name="auth/index"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: "",
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => router.back()}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="auth/login"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: "",
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => router.back()}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="auth/phone"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={0} totalSteps={4} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => router.back()}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="auth/email"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={0} totalSteps={4} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => router.back()}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="auth/verification"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={1} totalSteps={4} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => router.back()}
+										/>
+									),
+								}}
+							/>
 
-					<Stack.Screen
-						name="(onboarding)"
-						options={{
-							headerShown: false,
-						}}
-					/>
+							<Stack.Screen
+								name="(onboarding)"
+								options={{
+									headerShown: false,
+								}}
+							/>
 
-					<Stack.Screen
-						name="complete-profile/index"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={0} totalSteps={5} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => {
-										if (router.canGoBack()) {
-											router.back();
-										} else {
-											router.replace("/(tabs)");
-										}
-									}}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="complete-profile/pick-avatar"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={1} totalSteps={5} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => {
-										if (router.canGoBack()) {
-											router.back();
-										} else {
-											router.replace("/(tabs)");
-										}
-									}}
-								/>
-							),
-						}}
-					/>
+							<Stack.Screen
+								name="complete-profile/index"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={0} totalSteps={5} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => {
+												if (router.canGoBack()) {
+													router.back();
+												} else {
+													router.replace("/(tabs)");
+												}
+											}}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="complete-profile/pick-avatar"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={1} totalSteps={5} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => {
+												if (router.canGoBack()) {
+													router.back();
+												} else {
+													router.replace("/(tabs)");
+												}
+											}}
+										/>
+									),
+								}}
+							/>
 
-					<Stack.Screen
-						name="complete-profile/profile-details"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={2} totalSteps={5} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => {
-										if (router.canGoBack()) {
-											router.back();
-										} else {
-											router.replace("/(tabs)");
-										}
-									}}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="complete-profile/services"
-						options={{
-							headerShown: true,
-							headerStyle: { backgroundColor: "#000" },
-							headerTitle: () => <ProgressBar currentStep={3} totalSteps={5} />,
-							headerLeft: () => (
-								<Ionicons
-									name="chevron-back"
-									size={24}
-									color="white"
-									onPress={() => {
-										if (router.canGoBack()) {
-											router.back();
-										} else {
-											router.replace("/(tabs)");
-										}
-									}}
-								/>
-							),
-						}}
-					/>
-					<Stack.Screen
-						name="(chats)"
-						options={{
-							headerShown: false,
-							headerTitle: "",
-							headerStyle: { backgroundColor: "#000" },
-						}}
-					/>
-					<Stack.Screen
-						name="create-vehicle"
-						options={{
-							headerShown: false,
-						}}
-					/>
+							<Stack.Screen
+								name="complete-profile/profile-details"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={2} totalSteps={5} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => {
+												if (router.canGoBack()) {
+													router.back();
+												} else {
+													router.replace("/(tabs)");
+												}
+											}}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="complete-profile/services"
+								options={{
+									headerShown: true,
+									headerStyle: { backgroundColor: "#000" },
+									headerTitle: () => <ProgressBar currentStep={3} totalSteps={5} />,
+									headerLeft: () => (
+										<Ionicons
+											name="chevron-back"
+											size={24}
+											color="white"
+											onPress={() => {
+												if (router.canGoBack()) {
+													router.back();
+												} else {
+													router.replace("/(tabs)");
+												}
+											}}
+										/>
+									),
+								}}
+							/>
+							<Stack.Screen
+								name="(chats)"
+								options={{
+									headerShown: false,
+									headerTitle: "",
+									headerStyle: { backgroundColor: "#000" },
+								}}
+							/>
+							<Stack.Screen
+								name="create-vehicle"
+								options={{
+									headerShown: false,
+								}}
+							/>
 
-					<Stack.Screen
-						name="(tabs)"
-						options={{
-							headerShown: false,
-							headerStyle: { backgroundColor: "#000" },
-						}}
-					/>
-				</Stack>
+							<Stack.Screen
+								name="(tabs)"
+								options={{
+									headerShown: false,
+									headerStyle: { backgroundColor: "#000" },
+								}}
+							/>
+						</Stack>
+					</View>
 			</ThemeProvider>
 		</QueryClientProvider>
 	);
