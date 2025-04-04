847ca07c754d0bd6c8ca416f455bb3d48a0f621c
David <david@kamealabs.com>
2025-04-04 14:54:39 +0200

    feat: apple auth

--- src/app/auth/index.tsx
+++ src/app/auth/index.tsx
@@ -1,10 +1,13 @@
-import { StatusBar, Text, View } from "react-native";
+import { Platform, StatusBar, Text, View } from "react-native";
 
 import { Button } from "@/components/ui/button";
-import { IconApple } from "@/components/vectors/icon-apple";
+
+import * as AppleAuthentication from 'expo-apple-authentication'
+
 import { IconGoogle } from "@/components/vectors/icon-google";
 import { IconPhone } from "@/components/vectors/icon-phone";
 import { LogoB } from "@/components/vectors/logo-b";
+import { supabase } from "@/lib/supabase";
 import { Ionicons } from "@expo/vector-icons";
 import { router } from "expo-router";
 
@@ -38,16 +41,54 @@ export default function Auth() {
 							router.push("/auth/email");
 						}}
 					/>
+					{Platform.OS === "ios" && (
+						<AppleAuthentication.AppleAuthenticationButton
+							buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+							buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.WHITE}
+							cornerRadius={5}
+							style={{ width: "100%", height: 44 }}
+							onPress={async () => {
+								try {
+									const credential = await AppleAuthentication.signInAsync({
+										requestedScopes: [
+											AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+											AppleAuthentication.AppleAuthenticationScope.EMAIL,
+										],
+									})
+									// Sign in via Supabase Auth.
+									if (credential.identityToken) {
+										const {
+											error,
+											data: { user },
+										} = await supabase.auth.signInWithIdToken({
+											provider: 'apple',
+											token: credential.identityToken,
+										})
+
+										console.log(JSON.stringify({ error, user }, null, 2))
+
+										if (!error) {
+											router.push("/(onboarding)");
+										}
+									} else {
+										throw new Error('No identityToken.')
+									}
+								} catch (e) {
+									if (e instanceof Error && e.message === 'ERR_REQUEST_CANCELED') {
+										// handle that the user canceled the sign-in flow
+										alert("User canceled the sign-in flow");
+									} else {
+										// handle other errors
+										alert("An error occurred");
+									}
+								}
+							}}
+						/>
+					)}
 					<Button
 						variant="with-icon"
 						label="Avec Google"
 						icon={<IconGoogle />}
-						onPress={() => {}}
-					/>
-					<Button
-						variant="with-icon"
-						label="Avec Apple"
-						icon={<IconApple />}
 						onPress={() => {}}
 					/>
 				</View>
